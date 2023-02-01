5e899b9fdcd79f77959a6582873525834ee03acb
Yi Yao <leaf_yao8621@hotmail.com>
2023-02-01 15:51:15 -0800

    Bug fix

--- src/util/errcode.c
+++ src/util/errcode.c
@@ -2,7 +2,7 @@
 
 const char *http_header_errcode_lookup[6] = {
     "HTTP_HEADER_ERR_OK",
-    "HTTP_HEADER_ERR_NULL_PTR"
+    "HTTP_HEADER_ERR_NULL_PTR",
     "HTTP_HEADER_ERR_INITIALIZE",
     "HTTP_HEADER_ERR_PARSE",
     "HTTP_HEADER_ERR_ILL_FORMATED",
