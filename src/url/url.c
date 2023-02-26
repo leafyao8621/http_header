e8b25a1700d13b69f4df2462676a0abcc37fb854
Yi Yao <leaf_yao8621@hotmail.com>
2023-02-25 16:49:19 -0800

    Added decode util

--- src/url/url.c
+++ src/url/url.c
@@ -5,6 +5,44 @@
 
 DEF_DARRAY_FUNCTIONS(String)
 
+int url_decode(char *in, String *out) {
+    char chr = 0;
+    int ret = 0;
+    for (char *iter = in; *iter; ++iter) {
+        if (*iter == '%') {
+            chr = 0;
+            for (int i = 0; i < 2; ++i) {
+                ++iter;
+                chr <<= 4;
+                if (*iter >= '0' && *iter <= '9') {
+                    chr += *iter - '0';
+                } else if (*iter >= 'A' && *iter <= 'F') {
+                    chr += *iter - 'A' + 10;
+                } else if (*iter >= 'a' && *iter <= 'f') {
+                    chr += *iter - 'a' + 10;
+                } else {
+                    return HTTP_UTIL_ILL_FORMATTED;
+                }
+            }
+            ret = DArrayChar_push_back(out, &chr);
+            if (ret) {
+                return HTTP_UTIL_ERR_PARSE;
+            }
+        } else {
+            ret = DArrayChar_push_back(out, iter);
+            if (ret) {
+                return HTTP_UTIL_ERR_PARSE;
+            }
+        }
+    }
+    chr = 0;
+    ret = DArrayChar_push_back(out, &chr);
+    if (ret) {
+        return HTTP_UTIL_ERR_PARSE;
+    }
+    return 0;
+}
+
 int URL_initialize(URL *url) {
     if (!url) {
         return HTTP_UTIL_ERR_NULL_PTR;
